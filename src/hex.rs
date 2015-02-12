802be60ef91e5eb55e72d66b567e1aacd74145c8
James Sanderson <zofrex@gmail.com>
2015-02-12 00:12:06 +0000

    Handle padding for truncated triplets

--- src/hex.rs
+++ src/hex.rs
@@ -37,6 +37,18 @@ pub fn int_triplet_to_base64(triplet: &[u8; 3]) -> Vec<char> {
     indexes.iter().map(|i| index_base64(*i)).collect()
 }
 
+pub fn int_couplet_to_base64(couplet: &[u8; 2]) -> Vec<char> {
+    let mut result = int_triplet_to_base64(&[couplet[0], couplet[1], 0]);
+    result[3] = '=';
+    result
+}
+
+pub fn int_singlet_to_base64(singlet: &[u8; 1]) -> Vec<char> {
+    let mut result = int_couplet_to_base64(&[singlet[0], 0]);
+    result[2] = '=';
+    result
+}
+
 #[test]
 fn all_hexes() {
     let hexes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
@@ -88,4 +100,14 @@ fn look_up_base64_too_large() {
 #[test]
 fn test_triplet_to_base64() {
     assert_eq!(int_triplet_to_base64(&[77, 97, 110]), vec!['T', 'W', 'F', 'u']);
+}
+
+#[test]
+fn test_couplet_to_base64() {
+    assert_eq!(int_couplet_to_base64(&[77, 97]), vec!['T', 'W', 'E', '=']);
+}
+
+#[test]
+fn test_singlet_to_base64() {
+    assert_eq!(int_singlet_to_base64(&[77]), vec!['T', 'Q', '=', '=']);
 }
